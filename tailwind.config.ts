f45a2786584a55cd65447aedb791a6455fd175ae
sezaru-dev <cezarbaguhin20@gmail.com>
2025-02-01 15:44:04 +0800

    Initial commit with reset styling

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,12 +7,29 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+    colors: {
+      /* PRIMARY */
+      blue: 'hsl(246, 80%, 60%)',
+
+      orange: 'hsl(15, 100%, 70%)',
+      softBlue: 'hsl(195, 74%, 62%)',
+      lightRed: 'hsl(348, 100%, 68%)',
+      limeGreen: 'hsl(145, 58%, 55%)',
+      violet: 'hsl(264, 64%, 52%)',
+      softOrange: 'hsl(43, 84%, 65%)',
+
+      /* NEUTRAL */
+
+      veryDarkBlue: 'hsl(226, 43%, 10%)',
+      darkBlue: 'hsl(235, 46%, 20%)',
+      desaturatedBlue: 'hsl(235, 45%, 61%)',
+      paleBlue: 'hsl(236, 100%, 87%)',
+      
+          },
+    fontFamily: {
+      Rubik: ["Rubik", "sans-serif"],
     },
+    extend: {},
   },
   plugins: [],
 };
